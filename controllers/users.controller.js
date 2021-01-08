59b9200a8a3f0c8fdca421bfcde071baeafc4a74
Guilherme  Costa <guilhermecosta@AirdeGuilherme.lan>
2021-01-08 21:53:47 +0000

    users done

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,7 +2,7 @@ const con = require("../connection")
 const messages = require("../messages")
 
 async function getUsers(req, res) {
-    const query = "select id_user, name from users;"
+    const query = "select * from users;"
     con.query(query, (err, results, fields) => {
         if (err) {
             return res.status(messages.error().status).send(messages.error("error", err.sqlMessage))
@@ -12,8 +12,8 @@ async function getUsers(req, res) {
 }
 
 async function addUser(req, res) {
-    const { name, password} = req.body
-    const query = `insert into users (name, password) values ("${name}", "${password}")`
+    const { username, password, roleId} = req.body
+    const query = `insert into users (username, password, role_id) values ("${username}", "${password}", "${roleId}")`
     con.query(query, (err, results, fields) => {
         if (err) {
             return res.status(messages.error().status).send(messages.error("error", err.sqlMessage))
@@ -35,13 +35,16 @@ async function deleteUser(req, res) {
 
 async function editUser(req, res) {
     const { id } = req.params
-    const { name, password} = req.body
+    const { username, password, roleId} = req.body
     let set = []
-    if (name) {
-        set.push(`name = "${name}"`) 
+    if (username) {
+        set.push(`username = "${username}"`) 
     }
     if (password) {
         set.push(`password = "${password}"`) 
+    }
+    if (roleId) {
+        set.push(`password = "${roleId}"`) 
     }
     
     const query = `update users set ${set.join()} where id_user = ${id}`
