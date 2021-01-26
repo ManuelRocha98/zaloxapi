e095944274944c794adb939c0ffb69c836cdc799
Manuel Rocha <36920329+ManuelRocha98@users.noreply.github.com>
2021-01-26 21:54:49 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const controller = require('./controllers/messages.controller')
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -34,6 +34,6 @@ io.on('connection', (socket) => {
 
 require("./routes/index")(app)
 
-http.listen(3000, () => {
+http.listen(process.env.PORT || 3000, () => {
     console.log('Node app is running on port 3000')
 });
